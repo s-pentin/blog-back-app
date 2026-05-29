7c1f4f99aa9a3cdb05e87796702743b56f85d865
s-pentin <s5.pentin@yandex.ru>
2026-05-29 16:14:50 +0300

    Добавление файлового хранилища

--- src/main/java/org/blog/config/AppConfig.java
+++ src/main/java/org/blog/config/AppConfig.java
@@ -7,13 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
+@EnableTransactionManagement
 @PropertySource("classpath:config/db.properties")
 @ComponentScan(basePackages = {"org.blog.controllers", "org.blog.services", "org.blog.repository"})
 public class AppConfig {
@@ -26,12 +33,19 @@ public class AppConfig {
     private String password;
     @Value("${db.driver}")
     private String driver;
+    @Value("${images.storage.path}")
+    private String imagesPath;
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource(url, username, password);
         ds.setDriverClassName(driver);
         return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean
@@ -48,5 +62,12 @@ public class AppConfig {
         initializer.setDataSource(dataSource);
         initializer.setDatabasePopulator(populator);
         return initializer;
+    }
+
+    @Bean
+    public Path imagesStoragePath() throws IOException {
+        Path path = Path.of(imagesPath.isBlank() ? "./images" : imagesPath);
+        Files.createDirectories(path);
+        return path;
     }
 }
