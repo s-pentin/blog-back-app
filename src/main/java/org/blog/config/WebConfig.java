31a7dc501456d868713b323e196f522a4e388832
s-pentin <s5.pentin@yandex.ru>
2026-05-29 16:16:15 +0300

    Настройка источников CORS

--- src/main/java/org/blog/config/WebConfig.java
+++ src/main/java/org/blog/config/WebConfig.java
@@ -1,7 +1,11 @@
 package org.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,4 +16,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.blog.config"
 })
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost", "http://localhost:80")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
