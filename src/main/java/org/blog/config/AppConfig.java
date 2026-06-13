277c59293b84088bfeb4c04f6467823601ed365e
s-pentin <s5.pentin@yandex.ru>
2026-06-13 19:06:58 +0300

    Add image storage configuration

--- src/main/java/org/blog/config/AppConfig.java
+++ src/main/java/org/blog/config/AppConfig.java
@@ -2,71 +2,20 @@ package org.blog.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Configuration
-@EnableTransactionManagement
-@PropertySource("classpath:config/db.properties")
-@ComponentScan(basePackages = {"org.blog.controllers", "org.blog.services", "org.blog.repository"})
 public class AppConfig {
 
-    @Value("${db.url}")
-    private String url;
-    @Value("${db.username}")
-    private String username;
-    @Value("${db.password}")
-    private String password;
-    @Value("${db.driver}")
-    private String driver;
     @Value("${images.storage.path}")
-    private String imagesPath;
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource(url, username, password);
-        ds.setDriverClassName(driver);
-        return ds;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(new ClassPathResource("schema.sql"));
-        populator.setIgnoreFailedDrops(true);
-        DataSourceInitializer initializer = new DataSourceInitializer();
-        initializer.setDataSource(dataSource);
-        initializer.setDatabasePopulator(populator);
-        return initializer;
-    }
+    private String imagesStoragePath;
 
     @Bean
     public Path imagesStoragePath() throws IOException {
-        Path path = Path.of(imagesPath.isBlank() ? "./images" : imagesPath);
+        Path path = Path.of(imagesStoragePath);
         Files.createDirectories(path);
         return path;
     }
